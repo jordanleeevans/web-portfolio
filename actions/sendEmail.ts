ddc6cbd2a2d7c1af4788e8e60df6b799c326c6f4
Jordan Evans <jordan.evans@yard.email>
2024-03-22 16:30:53 +0000

    Add sendEmail function to handle email sending

---
+++ actions/sendEmail.ts
@@ -0,0 +1,44 @@
+"use server";
+import { React } from "react";
+import { Resend } from "resend";
+import { isValidString, getErrorMessage } from "@/lib/helpers";
+import { ContactFormEmail } from "@/components/email/contact-form-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const senderMessage = formData.get("senderMessage");
+
+  if (!isValidString(senderEmail, 100)) {
+    return {
+      error: "Invalid email",
+    };
+  }
+
+  if (!isValidString(senderMessage, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Web Portfolio Contact <onboarding@resend.dev>",
+      to: "jordanleeevans@hotmail.com",
+      subject: "New message from your website",
+      reply_to: senderEmail as string,
+      // react: React.createElement(ContactFormEmail, { senderMessage }),
+      text: senderMessage as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  } finally {
+    return {
+      status: 200,
+      body: "Message sent successfully, whoop whoop! 🎉",
+    };
+  }
+};
