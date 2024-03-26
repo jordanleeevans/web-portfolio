7beaef2d8d1c590462b60026b1bd9ae56294005e
Jordan Evans <jordan.evans@yard.email>
2024-03-26 00:10:06 +0000

    Update theme toggle button styles

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -8,7 +8,7 @@ export default function ThemeToggle() {
 
   return (
     <button
-      className="fixed bottom-4 right-4 p-2 my-2 w-[3rem] h-[3rem] dark:bg-gray-800 backdrop-blur-md rounded-full bg-opacity-80 dark:text-white border-white border-opacity-40 shadow-2xl flex items-center justify-center hover:scale-105 active:scale-110 transition-all"
+      className="fixed bottom-4 right-4 p-2 my-2 w-[3rem] h-[3rem] dark:bg-gray-800 rounded-full bg-gray-250 dark:text-white border-2 border-black/10 dark:border-white/10 shadow-2xl flex items-center justify-center hover:scale-105 active:scale-110 transition-all"
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun size={24} /> : <BsMoon size={24} />}
