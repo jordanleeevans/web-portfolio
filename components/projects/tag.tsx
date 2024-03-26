8fa27ee8532ebe8445cedecf3552015be870c1e2
Jordan Evans <jordan.evans@yard.email>
2024-03-26 21:07:53 +0000

    Refactor Tag component to use TagProps interface

--- components/projects/tag.tsx
+++ components/projects/tag.tsx
@@ -1,4 +1,6 @@
-export default function Tag({ tag }) {
+import { TagProps } from "@/lib/types";
+
+export default function Tag({ tag }: TagProps) {
   return (
     <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full">
       {tag}
