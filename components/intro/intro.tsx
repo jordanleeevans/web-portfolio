9bcc974e6699a201d636dcc67c7b991ad75fd35f
Jordan Evans <jordan.evans@yard.email>
2024-03-27 00:48:27 +0000

    Refactor intro component to use useThemeToggleReminder hook

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -2,12 +2,23 @@
 import Profile from "@/components/intro/profile";
 import IntroText from "@/components/intro/text";
 import ActionButtons from "@/components/intro/buttons";
-import { useDarkModeReminder } from "@/hooks/useDarkModeReminder";
+import { useThemeToggleReminder } from "@/hooks/useThemeToggleReminder";
 import useSectionInView from "@/hooks/useSectionInView";
+import { useActiveTheme } from "@/context/active-theme-context";
 
 export default function Intro() {
+  const { theme } = useActiveTheme();
   const { ref } = useSectionInView("Home", 0.5);
-  useDarkModeReminder("Remember, there is a dark mode available!", 3000); // 3 seconds delay
+
+  const toggleReminderParams = {
+    message:
+      theme === "dark" ? "Check out light mode!" : "Check out dark mode!",
+    icon: theme === "dark" ? "☀️" : "🌙",
+    delay: 3000,
+  };
+
+  useThemeToggleReminder(toggleReminderParams);
+
   return (
     <section
       ref={ref}
